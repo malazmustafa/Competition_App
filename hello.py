a17ee0979d2a2208d6d4b4bcdcbfb8e709a273c6
Anastasia <anastasia-aniwa@live.com>
2019-03-25 23:06:19 +0000

    tesr

--- hello.py
+++ hello.py
@@ -11,6 +11,21 @@ def send_simple_message(email, username):
         data={"from": "Excited User <postmaster@sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org>",
         "to": [email],
         "subject": "Hello " + username,
-        "text": "Hehe look i learnt how to add HTML and attachments to emails! ",
-        "html": "<html> Hehe look i learnt how to add <b>HTML</b> and <i>attachments</i> to emails!</html>",
-        "o:deliverytime": "Sun, 17 Mar 2019 16:12:00 -0000"})
+        "text": "Welcome! ",
+        "html": "<html><b>Welcome!</b></html>",
+        })
+
+
+
+@app.route("/") #@app.route specifies what path to take to find a function
+def hello_someone():
+    return render_template("signup.html")
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.form
+    print form_data["email"]
+    send_simple_message (form_data["email"], form_data["username"]) #form_data is a variable to help you store information
+    return "All OK"
+
+    app.run(debug=True, port=1235)
