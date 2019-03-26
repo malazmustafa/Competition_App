42fa9253db9b144b03a5502f17dc1ed1d6b5049f
Anastasia <anastasia-aniwa@live.com>
2019-03-26 18:18:01 +0000

    fixed mailgun

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ def send_simple_message(email, firstname):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org/messages",
         auth=("api", "b5e6c9e56fab046908f1e9273b944409-de7062c6-e7190b95"),
-        files= [("attachment", ("test.jpg", open("/Users/anastasiaaniwa/Desktop/CodeFirstGirls/Python/my_app/static/images/test.jpg", "rb").read
+        files= [("attachment", ("hello.jpg", open("/Users/anastasiaaniwa/Desktop/CodeFirstGirls/Python/Competition_App/static/images/hello.jpg.jpg", "rb").read
         ()))],
         data={"from": "Excited User <postmaster@sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org>",
         "to": [email],
@@ -17,15 +17,15 @@ def send_simple_message(email, firstname):
 
 
 
-@app.route("/") #@app.route specifies what path to take to find a function
+@app.route("/index.html") #@app.route specifies what path to take to find a function
 def hello_someone():
     return render_template("index.html")
 
-@app.route("/", methods=["POST"])
+@app.route("/index", methods=["POST"])
 def sign_up():
     form_data = request.form
     print form_data["email"]
     send_simple_message (form_data["email"], form_data["firstname"]) #form_data is a variable to help you store information
     return "All OK"
 
-    app.run(debug=True, port=1235)
+app.run(debug=True, port=2345)
