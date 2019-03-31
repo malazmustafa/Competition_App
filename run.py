73fd09c1e69f49ad4b7dd08f3edb55b86e85f789
Anastasia <anastasia-aniwa@live.com>
2019-03-31 22:23:10 +0100

    added sign up page and fixed mailgun

--- run.py
+++ run.py
@@ -5,10 +5,10 @@ from main.hello import send_simple_message
 app = Flask("Competition_App")
 
 @app.route("/") #@app.route specifies what path to take to find a function
-def hello_someone():
+def home():
     return render_template("index.html")
 
-def send_simple_message(email, username):
+def send_simple_message(email, firstname):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org/messages",
         auth=("api", "b5e6c9e56fab046908f1e9273b944409-de7062c6-e7190b95"),
@@ -21,13 +21,16 @@ def send_simple_message(email, username):
         "html": "<html> Welcome to <i>ENCODE</i></html>",
         })
 
-@app.route("/index", methods=["POST"])
+@app.route("/") #@app.route specifies what path to take to find a function
+def hello_someone():
+    return render_template("signup.html")
+
+@app.route("/signup", methods=["POST"])
 def sign_up():
     form_data = request.form
-    print form_data["email"]
+    print (form_data["email"])
     send_simple_message (form_data["email"], form_data["firstname"]) #form_data is a variable to help you store information
     return "All OK"
-
 
 
 # port = int(os.environ.get('PORT', 5000))
