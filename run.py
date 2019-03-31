e9992f0d88c945b0eea1d2b6e6d4eebf2594fafd
Anastasia <anastasia-aniwa@live.com>
2019-03-31 21:10:13 +0100

    deployed to heroku and working on mailgun

--- run.py
+++ run.py
@@ -8,6 +8,19 @@ app = Flask("Competition_App")
 def hello_someone():
     return render_template("index.html")
 
+def send_simple_message(email, username):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org/messages",
+        auth=("api", "b5e6c9e56fab046908f1e9273b944409-de7062c6-e7190b95"),
+        files= [("attachment", ("test.jpg", open("/Users/anastasiaaniwa/Desktop/CodeFirstGirls/Python/my_app/static/images/test.jpg", "rb").read
+        ()))],
+        data={"from": "Excited User <postmaster@sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org>",
+        "to": [email],
+        "subject": "Hello " + username,
+        "text": "Hehe look i learnt how to add HTML and attachments to emails! ",
+        "html": "<html> Hehe look i learnt how to add <b>HTML</b> and <i>attachments</i> to emails!</html>",
+        "o:deliverytime": "Sun, 17 Mar 2019 16:12:00 -0000"})
+
 @app.route("/index", methods=["POST"])
 def sign_up():
     form_data = request.form
@@ -16,5 +29,8 @@ def sign_up():
     return "All OK"
 
 
-port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, port=port)
+
+# port = int(os.environ.get('PORT', 5000))
+# app.run(debug=True, port=port)
+port = int(os.environ.get('PORT', 2345))
+app.run(debug=True,host='0.0.0.0',port=port)
