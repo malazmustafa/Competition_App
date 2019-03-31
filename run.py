a8e78e45c273dfde59b44fb5936ec6ad5902dbbc
Anastasia <anastasia-aniwa@live.com>
2019-03-31 21:33:30 +0100

    changes text

--- run.py
+++ run.py
@@ -12,14 +12,14 @@ def send_simple_message(email, username):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org/messages",
         auth=("api", "b5e6c9e56fab046908f1e9273b944409-de7062c6-e7190b95"),
-        files= [("attachment", ("test.jpg", open("/Users/anastasiaaniwa/Desktop/CodeFirstGirls/Python/my_app/static/images/test.jpg", "rb").read
+        files= [("attachment", ("test.jpg", open("/Users/anastasiaaniwa/Desktop/CodeFirstGirls/Python/Competition_App/static/images/Welcome.png", "rb").read
         ()))],
         data={"from": "Excited User <postmaster@sandbox8ea045b272754b97a9a3b9100fd89d00.mailgun.org>",
         "to": [email],
-        "subject": "Hello " + username,
-        "text": "Hehe look i learnt how to add HTML and attachments to emails! ",
-        "html": "<html> Hehe look i learnt how to add <b>HTML</b> and <i>attachments</i> to emails!</html>",
-        "o:deliverytime": "Sun, 17 Mar 2019 16:12:00 -0000"})
+        "subject": "Hello " + firstname,
+        "text": "Welcome to ENCODE ",
+        "html": "<html> Welcome to <i>ENCODE</i></html>",
+        })
 
 @app.route("/index", methods=["POST"])
 def sign_up():
